8ed4ca2738fc08440265036b1600ce056231a548
wiktor-falek <falekwiktor@gmail.com>
2023-05-08 19:17:31 +0200

    Fix interface discrimination not working, set up stuff for publish

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,63 @@
-export function Ok<T = any>(value: T) {
-  const cb: Result<T> = {
+export type ResultOk<T> = {
+  val: T;
+  ok: true;
+};
+
+export type ResultErr = {
+  err: string;
+  ok: false;
+};
+
+export function Ok<T>(value: T) {
+  const cb: ResultOk<T> = {
     val: value,
-    isOk: () => true,
-    isErr: () => false,
+    ok: true,
   };
   return cb;
 }
 
 export function Err(message: string) {
-  const cb: Result<typeof message> = {
-    val: message,
-    isOk: () => false,
-    isErr: () => true,
+  const cb: ResultErr = {
+    err: message,
+    ok: false,
   };
   return cb;
 }
 
-export type Result<T> = {
-  val: T;
-  isOk: () => boolean;
-  isErr: () => boolean;
-};
+// export class Maybe {
+//   value: any;
+//   constructor(value: any) {
+//     this.value = value;
+//   }
 
+//   bind(func: (arg0: any) => any) {
+//     if (this.value === null) return this;
+
+//     if (this.value.ok === false) {
+//       return this;
+//     }
+
+//     if (this.value.ok === true) {
+//       let value = func(this.value.val);
+//       return new Maybe(value);
+//     }
+
+//     let value = func(this.value);
+//     return new Maybe(value);
+//   }
+// }
+
+// function divide(num: number, by: number) {
+//   return by === 0 ? Err("Cannot divide by 0") : Ok(num / by);
+// }
+
+// function addOne(val: number) {
+//   return val + 1;
+// }
+
+// const result = new Maybe(20)
+//   .bind((num) => divide(num, 2))
+//   .bind(addOne)
+//   .bind((num) => divide(num, 2));
+
+// console.log(result.value);
